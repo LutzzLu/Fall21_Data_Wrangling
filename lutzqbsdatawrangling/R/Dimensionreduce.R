d25289d0b268ba4bfabec1b5e7f68bbbd49ae604
LutzzLu <luyunrui.lutz@gmail.com>
2021-11-11 08:57:12 -0500

    change half r function

--- lutzqbsdatawrangling/R/Dimensionreduce.R
+++ lutzqbsdatawrangling/R/Dimensionreduce.R
@@ -1,3 +1,21 @@
+library(tsne)
+library(umap)
+library(randomForest)
+
+dimensionreduce = function(data, label){
+  data.pca = prcomp(data, center = TRUE)
+  dr.pca = data.frame(class = label[,1], dr1 = data.pca[, 1], dr2 = data.pca[, 2])
+  data.tsne = tsne(data, k=2, perplexity=50)
+  dr.tsne = data.frame(class = label[, 1], dr1 = data.tsne[, 1], dr2 = data.tsne[, 2])
+  data.umap.matrix = as.matrix(data)
+  data.umap = umap(data.umap.matrix)
+  dr.umap = data.frame(class = label[, 1], dr1 = data.umap[, 1], dr2 = data.umap[, 2])
+
+  dr.pca$class = as.factor(dr.pca$class)
+  return()
+}
+
+
 hello <- function() {
   print("Hello, world!")
 }
