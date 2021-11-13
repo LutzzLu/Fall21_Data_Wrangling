d525e743517bc6089d2899c38de6933453725dc3
LutzzLu <luyunrui.lutz@gmail.com>
2021-11-13 11:42:20 -0500

    fix package

--- lutzqbsdatawrangling/R/Dimensionreduce.R
+++ lutzqbsdatawrangling/R/Dimensionreduce.R
@@ -4,7 +4,7 @@ library(randomForest)
 
 dimensionreduce = function(data, label){
   data.pca = prcomp(data, center = TRUE)
-  dr.pca = data.frame(class = label[,1], dr1 = data.pca[, 1], dr2 = data.pca[, 2])
+  dr.pca = data.frame(class = label[,1], dr1 = data.pca$x[, 1], dr2 = data.pca$x[, 2])
   data.tsne = tsne(data, k=2, perplexity=50)
   dr.tsne = data.frame(class = label[, 1], dr1 = data.tsne[, 1], dr2 = data.tsne[, 2])
   data.umap.matrix = as.matrix(data)
