1e470c67203a91f00f5167a29ee8412c424b7804
LutzzLu <luyunrui.lutz@gmail.com>
2021-11-13 15:48:11 -0500

    test other viz

--- lutzqbsdatawrangling/R/viz_ggplot.R
+++ lutzqbsdatawrangling/R/viz_ggplot.R
@@ -9,18 +9,18 @@ viz.ggplot <- function(data) {
     labs(x="dr_1",y="dr_2")+
     theme_classic()
 
-  mean1=c()
-  mean2=c()
-  var1=c()
-  var2=c()
-  for(i in 1:length(T)){
-    mean1[i]=mean(data[which(data$target==T[i]),]$dr_1)
-    mean2[i]=mean(data[which(data$target==T[i]),]$dr_2)
-    var1[i]=var(data[which(data$target==T[i]),]$dr_1)
-    var2[i]=var(data[which(data$target==T[i]),]$dr_2)
-  }
-  measure=data.frame(T,mean1,mean2,var1,var2)
-  head(measure)
-  plot(density(data$dr_1))
-  lines(density(data$dr_2),col="red")
+  # mean1=c()
+  # mean2=c()
+  # var1=c()
+  # var2=c()
+  # for(i in 1:length(T)){
+  #   mean1[i]=mean(data[which(data$target==T[i]),]$dr_1)
+  #   mean2[i]=mean(data[which(data$target==T[i]),]$dr_2)
+  #   var1[i]=var(data[which(data$target==T[i]),]$dr_1)
+  #   var2[i]=var(data[which(data$target==T[i]),]$dr_2)
+  # }
+  # measure=data.frame(T,mean1,mean2,var1,var2)
+  # head(measure)
+  # plot(density(data$dr_1))
+  # lines(density(data$dr_2),col="red")
 }
