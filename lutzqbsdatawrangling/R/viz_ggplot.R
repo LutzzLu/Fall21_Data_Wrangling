4b4708d6fda5e7554e392dde24b7939fe9a4aeb0
LutzzLu <luyunrui.lutz@gmail.com>
2021-11-13 15:42:37 -0500

    add test

--- lutzqbsdatawrangling/R/viz_ggplot.R
+++ lutzqbsdatawrangling/R/viz_ggplot.R
@@ -1,4 +1,4 @@
-viz.ggplot <- function(pca) {
+viz.ggplot <- function(data) {
   # library(ggplot2)
   # colnames(data)=c("target","dr_1","dr_2")
   # T=unique(data$target)
@@ -23,27 +23,27 @@ viz.ggplot <- function(pca) {
   # lines(density(data$dr_2),col="red")
 
   library(ggplot2)
-  colnames(pca)=c("target","PCA_1","PCA_2")
-  T=unique(pca$target)
+  colnames(data)=c("target","dr_1","dr_2")
+  T=unique(data$target)
 
 
-  ggplot(pca,aes(x=`PCA_1`,y=`PCA_2`,color=target))+
+  ggplot(data,aes(x=`dr_1`,y=`dr_2`,color=target))+
     geom_point()+ 
-    labs(x="PCA_1",y="PCA_2")+
+    labs(x="dr_1",y="dr_2")+
     theme_classic()
 
-  pca_mean1=c()
-  pca_mean2=c()
-  pca_var1=c()
-  pca_var2=c()
+  mean1=c()
+  mean2=c()
+  var1=c()
+  var2=c()
   for(i in 1:length(T)){
-    pca_mean1[i]=mean(pca[which(pca$target==T[i]),]$PCA_1)
-    pca_mean2[i]=mean(pca[which(pca$target==T[i]),]$PCA_2)
-    pca_var1[i]=var(pca[which(pca$target==T[i]),]$PCA_1)
-    pca_var2[i]=var(pca[which(pca$target==T[i]),]$PCA_2)
+    mean1[i]=mean(data[which(data$target==T[i]),]$dr_1)
+    mean2[i]=mean(data[which(data$target==T[i]),]$dr_2)
+    var1[i]=var(data[which(data$target==T[i]),]$dr_1)
+    var2[i]=var(data[which(data$target==T[i]),]$dr_2)
   }
-  pcameasure=data.frame(T,pca_mean1,pca_mean2,pca_var1,pca_var2)
-  head(pcameasure)
-  plot(density(pca$PCA_1))
-  lines(density(pca$PCA_2),col="red")
+  measure=data.frame(T,mean1,mean2,var1,var2)
+  head(measure)
+  plot(density(data$dr_1))
+  lines(density(data$dr_2),col="red")
 }
